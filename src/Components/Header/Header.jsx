2bbfb1cadc3d712de9ad5beb3a4a1c3d945b46b1
Jayme Liao <jaymeliao1226@gmail.com>
2024-01-30 20:23:01 -0800

    huhu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,48 +3,57 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faComment, faBell } from "@fortawesome/free-solid-svg-icons";
+import "./Header.scss";
 
 function Header() {
   return (
-    <header class="bg-white shadow p-4 flex items-center justify-between">
-      <div class="flex items-center">
-        <div class="bg-gray-300 h-8 w-32 rounded mx-4"></div>
-        <div class="hidden sm:block ml-4 relative mx-4">
+    <header className="bg-white shadow p-4 flex items-center justify-between">
+      <div className="flex items-center">
+        <div className="bold mx-4"><h1>CodeSpotLight</h1></div>
+        <div className="hidden sm:block ml-4 relative mx-4">
           <input
             type="search"
             placeholder="Search"
-            class="pl-10 pr-4 py-2 border rounded-full w-64"
+            className="pl-10 pr-4 py-2 border rounded-full w-64"
           />
-          <div class="absolute left-0 top-0 mt-2 ml-3">
-            <div class="icon"></div>
+          <div className="absolute left-0 top-0 mt-2 ml-3">
+            <div className="input-inner-icon"></div>
           </div>
         </div>
       </div>
-      <nav class="flex items-center">
-        <a href="#" class="mx-4">
-          <FontAwesomeIcon
-            icon={faPlus}
-            className="h-5 w-5"
-            style={{ color: "#d1d5db" }}
-          />
-        </a>
-        <a href="#" class="mx-4">
-          <div class="icon">
-            <div class="icon">
-              <FontAwesomeIcon className="h-5 w-5" icon={faComment} style={{ color: "#d1d5db" }}/>
-            </div>
+      <nav className="flex items-center">
+      <a href="#" className="mx-4">
+          <div className="icon h-10 w-10 rounded-full">
+            <FontAwesomeIcon
+              className="h-5 w-5"
+              icon={faPlus}
+              style={{ color: "black" }}
+            />
           </div>
         </a>
-        <a href="#" class="mx-4">
-          <div class="icon">
-            <FontAwesomeIcon className="h-5 w-5" icon={faBell} />
+        <a href="#" className="mx-4">
+          <div className="icon h-10 w-10 rounded-full">
+            <FontAwesomeIcon
+              className="h-5 w-5"
+              icon={faComment}
+              style={{ color: "black" }}
+            />
           </div>
         </a>
-        <a href="#" class="hidden sm:mx-4">
-          <div class="icon">
+        <a href="#" className="mx-4">
+          <div className="icon h-10 w-10 rounded-full">
+            <FontAwesomeIcon
+              className="h-5 w-5"
+              icon={faBell}
+              style={{ color: "black" }}
+            />
+          </div>
+        </a>
+        <a href="#" className="hidden sm:inline-block sm:mx-4">
+          <div>
             <img
               className="h-10 w-10 rounded-full"
-              src="https://unsplash.it/100/100?image=1020"
+              src="https://unsplash.it/100/100?image=1021"
               alt="Profile picture"
             />
           </div>
